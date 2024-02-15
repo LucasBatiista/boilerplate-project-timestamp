2e638332e3e950c82a4cc5344a895c2729aa7575
Lucas Batista <lucasjsbatista@gmail.com>
2024-02-15 11:31:49 -0400

    All functionalities applied

--- index.js
+++ index.js
@@ -24,6 +24,21 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date", (req, res) => {
+  
+  const date_format = /\b\d{4}-\d{2}-\d{2}\b/g;
+  if (req.params.date.match(date_format)) {
+    date = new Date(req.params.date);
+  } else {
+    date = new Date(Number(req.params.date));
+  }
+  res.json({unix: date.getTime(), utc: date.toUTCString()});
+})
+
+app.get("/api", (req, res) => {
+  timestamp = new Date();
+  res.json({unix: timestamp.getTime(), utc: timestamp.toUTCString()});
+})
 
 
 // Listen on port set in environment variable or default to 3000
